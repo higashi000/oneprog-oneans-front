254295a2537f34641eb20bcdd60b2fe1cbab39a5
higashi000 <higashi.ict@gmail.com>
2020-04-09 17:28:24 +0900

    Fix file path

--- src/getSetlist/getsetlist.ts
+++ src/getSetlist/getsetlist.ts
@@ -7,7 +7,7 @@ AXIOS.get(ORIGIN_URL + '/oneprog-oneans/getsetlist')
     let elem: HTMLInputElement = <HTMLInputElement>document.getElementById('setlist');
 
     response.data.forEach(function(value) {
-      let form = '<a value="' + value._id + '" href="../challenge/index.html?id=' + value._id + '&num=0">' + value.title + '</a><br>';
+      let form = '<a value="' + value._id + '" href="../challenge.html?id=' + value._id + '&num=0">' + value.title + '</a><br>';
       elem.insertAdjacentHTML('beforeend', form);
     })
   })
